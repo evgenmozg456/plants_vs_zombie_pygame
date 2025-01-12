5d041acecf942afaf62504569ef2b1c63170f1d7
timatun <timatun@yandex.ru>
2025-01-12 09:48:16 +0300

    Добавлено меню + лопата, которая может удалять. ТАкже упрощён код

--- Board.py
+++ Board.py
@@ -1,14 +1,12 @@
 import pygame
-from pygame import font
-from load_image import load_image
 from Plants import Plants
-
 
 class Board(pygame.sprite.Sprite):
     # создание поля
     def __init__(self,size,  width, height, image_path, *groups):
         super().__init__(*groups)
         self.font = pygame.font.Font(None, 50)
+
 
         self.width = width
         self.height = height
@@ -20,20 +18,34 @@ class Board(pygame.sprite.Sprite):
         self.rect = self.image_pole.get_rect()
 
         self.all_sprites_plants = pygame.sprite.Group()
-
+        self.menu_sprites = pygame.sprite.Group()
 
         self.board = [[0] * width for _ in range(height)]
-        self.color_plants = ['black', 'yellow', 'green', 'brown', 'burlywood1', 'red', 'grey']  # в будущем тут будут спрайты растений
-        self.menu = [i for i in range(len(self.color_plants))]
+        self.sprites_menu = ['cards/podsolnux_card.jpg', 'cards/gorox_card.jpg', 'cards/potatomine_card.jpg',
+                             'cards/orex_card.jpg', 'cards/cherrybomb_card.jpg', 'cards/lopata.png']  #  спрайты менюшки
+        self.sprites_plants = ["plants/sunflower.png", 'plants/gorox_1.jpg',
+                               'plants/potatomine.png', "plants/orex.png",
+                               "plants/cherry.png"]
+        self.menu = [i for i in range(len(self.sprites_menu))]
 
         self.plants_choice = 0  # показывает какое растение выбрали
-        self.sun = 1000  # начальное кол-во солнышек
+        self.sun = 10000  # начальное кол-во солнышек
 
         # значения по умолчанию
         self.left = 350
         self.top = 150
         self.cell_size = 100
 
+        self.shovel_image = pygame.image.load("cards/lopata.png").convert_alpha()
+        self.shovel_active = False
+        self.shovel_rect = pygame.Rect(850, 0, 100, 100)
+
+        self._init_menu()
+
+    def _init_menu(self):
+        for i, menu_sprite in enumerate(self.sprites_menu):
+            menu = Plants(self.left + 100 * i, 0, menu_sprite)
+            self.menu_sprites.add(menu)
     def add_plant(self, plant):
         self.all_sprites_plants.add(plant)
 
@@ -44,60 +56,75 @@ class Board(pygame.sprite.Sprite):
         self.cell_size = cell_size
 
     def economica(self, plant=0):
-        if plant == 0:  # пассивынй доход
+        if plant == 0:
             self.sun += 25
         else:
-            # идёт проверка нужно ли ставить растения
-            if self.sun > 0:  # можно улучшить, но мне впадлу, зато работает
-                if self.plants_choice == 1:
-                    if self.sun - 50 >= 0:
-                        self.sun -= 50
-                        return True
-                if self.plants_choice == 2:
-                    if self.sun - 100 >= 0:
-                        self.sun -= 100
-                        return True
-                if self.plants_choice == 3:
-                    if self.sun - 25 >= 0:
-                        self.sun -= 25
-                        return True
-                if self.plants_choice == 4:
-                    if self.sun - 50 >= 0:
-                        self.sun -= 50
-                        return True
-                if self.plants_choice == 5:
-                    if self.sun - 150 >= 0:
-                        self.sun -= 150
-                        return True
-            else:
-                # если не хватает денег(солнышек)
-                return False
+            costs = [0, 50, 100, 25, 50, 150]
+            if self.sun >= costs[self.plants_choice]:
+                self.sun -= costs[self.plants_choice]
+                return True
+        return False
 
     def render(self, screen):
         color = (255, 255, 255)
         screen.blit(self.image_pole, (0, 0))
 
+        self.menu_sprites.draw(screen)
+
         sun = self.font.render(f"{self.sun}", True, (255, 255, 255))
         name_sun = self.font.render("sun", True, (255, 255, 255))
-
         screen.blit(sun, (100, 50))
         screen.blit(name_sun, (110, 0))
 
-        for i in range(6):
-            pos_x = self.left + self.cell_size * i
-            pygame.draw.rect(screen, self.color_plants[i + 1], (pos_x, 0, self.cell_size, self.cell_size), 0)
+        if self.shovel_active:
+            pygame.mouse.set_visible(False)
+            mx, my = pygame.mouse.get_pos()
+            screen.blit(self.shovel_image, (mx - self.shovel_rect.width // 2, my - self.shovel_rect.height // 2))
+        else:
+            # Если лопата не активна, показываем стандартный курсор
+            pygame.mouse.set_visible(True)
 
         for j in range(self.width):
             for i in range(self.height):
                 pos_x = self.left + self.cell_size * j
                 pos_y = self.top + self.cell_size * i
-
                 # Рисуем сетку
                 pygame.draw.rect(screen, color, (pos_x, pos_y, self.cell_size, self.cell_size), 1)
 
         # Отображаем спрайты
         self.all_sprites_plants.draw(screen)
 
+    def handle_click(self, mouse_pos):
+        # print(f"Клик по координатам: {mouse_pos}")
+        """Обработка клика мыши."""
+        # Если клик по лопате, активируем или деактивируем её
+        if self.shovel_rect.collidepoint(mouse_pos):
+            self.shovel_active = not self.shovel_active
+            print("Клик на лопате!")
+            return
+
+        # Если лопата активна
+        if self.shovel_active:
+            cell = self.get_cell(mouse_pos)
+            if cell:
+                ay, ax = cell
+
+                # Если в клетке есть растение, удаляем его
+                if self.board[ay][ax] != 0:
+                    self.board[ay][ax] = 0  # Удаляем растение из логического поля
+
+                    # Удаляем растение из группы спрайтов
+                    for plant in self.all_sprites_plants:
+                        if plant.rect.collidepoint(mouse_pos):  # Проверяем, совпадает ли клик с растением
+                            self.all_sprites_plants.remove(plant)
+                            break
+
+            # Деактивируем лопату после использования
+            self.shovel_active = False
+            return
+
+        # Если лопата не активна, пытаемся поставить растение
+        self.get_click(mouse_pos)
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
         return cell
@@ -113,34 +140,16 @@ class Board(pygame.sprite.Sprite):
                 if self.economica(self.plants_choice):  # функция проверяет хавтает ли денег
                     self.board[ay][ax] = self.plants_choice
 
-                    x = self.left + ax * self.cell_size
-                    y = self.top + ay * self.cell_size
-
-                    if self.plants_choice == 1:
-                        gorox = Plants(x, y, "plants\sunflower.png")  # Создаем спрайт "подсолнуха"
-                        self.add_plant(gorox)
-
-                    if self.plants_choice == 2:
-                        gorox = Plants(x, y, "plants\gorox_1.jpg")  # Создаем спрайт "гороха"
-                        self.add_plant(gorox)  # Добавляем его в группу спрайтов
-
-                    if self.plants_choice == 3:
-                        gorox = Plants(x, y, "plants\potatomine.png")  # Создаем спрайт "подсолнуха"
-                        self.add_plant(gorox)
-
-                    if self.plants_choice == 4:
-                        gorox = Plants(x, y, "plants\orex.png")  # Создаем спрайт "подсолнуха"
-                        self.add_plant(gorox)
-                    if self.plants_choice == 5:
-                        gorox = Plants(x, y, "plants\cherry.png")  # Создаем спрайт "подсолнуха"
-                        self.add_plant(gorox)
-
+                    if self.plants_choice > 0:
+                        x = self.left + ax * self.cell_size
+                        y = self.top + ay * self.cell_size
+                        plant = Plants(x, y, self.sprites_plants[self.plants_choice - 1])  # Создаем спрайт
+                        self.add_plant(plant)
             return ay, ax
 
-        if 0 <= ax <= 4 and 0 <= my // self.cell_size <= (self.height - 1):  # определяется какое растение выбрано
+        if 0 <= ax <= 5 and 0 <= my // self.cell_size <= (self.height - 1):  # определяется какое растение выбрано
             self.plants_choice = ax + 1  # выбор растени из вверхней панели
             return ax
-
         else:
             return None
 
@@ -153,10 +162,11 @@ def main():
     clock = pygame.time.Clock()
     pygame.display.set_caption('Игровое поле')
     pole_image = 'pole.jpg'
-    gorox_image = 'gorox_1.jpg'
 
     size = 1900, 800
     screen = pygame.display.set_mode(size)
+
+    pygame.mouse.set_visible(True)
 
     board = Board(size, 10, 6, pole_image)
     running = True
@@ -166,7 +176,13 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # event.button == 1 - означает левую кнопку мыши
-                board.get_click(event.pos)
+                board.handle_click(event.pos)
+
+        # if board.shovel_active:
+        #     pygame.mouse.set_visible(False)
+        # else:
+        #     pygame.mouse.set_visible(True)
+
         screen.fill((0, 0, 0))
         board.render(screen)
 
@@ -176,8 +192,6 @@ def main():
             last_score_time = current_time
         pygame.display.flip()
         clock.tick(60)  # счётчик кадрор (fps)
-
-
 
 
 
